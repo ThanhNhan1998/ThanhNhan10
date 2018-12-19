650b63a7cd2531068c4cdeb252eac5ba8bcefba7
ThanhNhan1998 <moitapchojgame@gmail.com>
2018-12-19 22:10:13 +0700

    first commit

---
+++ src/baitap/BaiTap.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baitap;
+
+/**
+ *
+ * @author User
+ */
+public class BaiTap {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private static int n  = 0;
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        BaiTap a = new BaiTap();
+        BaiTap b = new BaiTap();
+        
+        System.out.println(n);
+    }
+    
+}
